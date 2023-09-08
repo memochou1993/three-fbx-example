6ffe9c5ee30e8d740897dbaa9db5f94bead412c6
Memo Chou <memochou1993@gmail.com>
2023-09-08 17:34:34 +0800

    Add example

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
-setupCounter(document.querySelector('#counter'))
+const light = new THREE.PointLight(0xffffff, 20)
+light.position.set(0.8, 1.4, 1.0)
+scene.add(light)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 5)
+scene.add(ambientLight)
+
+const camera = new THREE.PerspectiveCamera(
+  100,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+camera.position.set(0.8, 0.6, 1.0)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.target.set(0, 0.6, 0)
+
+const fbxLoader = new FBXLoader()
+fbxLoader.load(
+  'models/example.fbx',
+  (object) => {
+    object.scale.set(.005, .005, .005)
+    scene.add(object)
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+  },
+  (error) => {
+    console.log(error)
+  }
+)
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  render()
+}
+
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+function animate() {
+  requestAnimationFrame(animate)
+  controls.update()
+  render()
+  stats.update()
+}
+
+function render() {
+  renderer.render(scene, camera)
+}
+
+animate()
